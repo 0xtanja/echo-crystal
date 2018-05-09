9f77c2481be29ba26e7af3707f216b0ed03866e4
Mario Álvarez <ahoy@m4grio.me>
2018-05-09 18:03:17 +0800

    Initial version

---
+++ src/echo.cr
@@ -0,0 +1,25 @@
+require "http/server"
+require "./echo/*"
+
+module Echo
+
+  echo_port = ENV["ECHO_PORT"]?.try(&.to_i) || 8018
+  echo_host = ENV["ECHO_HOST"]?.try(&.to_s) || "127.0.0.1"
+
+  server = HTTP::Server.new(echo_host, echo_port, [
+    HTTP::LogHandler.new,
+  ]) do |ctx|
+    response = <<-TEMPLATE
+    #{ctx.request.method} #{ctx.request.path} #{ctx.request.version}
+    Request address: #{ctx.request.host}
+
+    TEMPLATE
+
+    ctx.response.content_type = "text/plain"
+    ctx.response.print response
+  end
+
+  puts "Listening on #{echo_host}:#{echo_port}"
+  server.listen
+
+end
